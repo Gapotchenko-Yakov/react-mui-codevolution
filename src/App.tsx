9bb50341ee6690c4234e80e6acb4c6c6b25206f9
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-03-27 21:30:11 +0300

    Add Select demo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import MuiButton from "./components/MuiButton";
-import MuiTextField from "./components/MuiTextField";
-import MuiTypography from "./components/MuiTypography";
+import MuiSelect from "./components/MuiSelect";
 
 function App() {
   return (
     <div className="App">
-      <MuiTextField />
+      <MuiSelect />
     </div>
   );
 }
