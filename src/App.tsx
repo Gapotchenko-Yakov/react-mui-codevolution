74e191b67aab2fa725e44fb995d29e6a2b893f58
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-03-28 22:34:21 +0300

    Add Menu as Popover to Navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import MuiLink from "./components/MuiLink";
+import MuiNavbar from "./components/MuiNavbar";
 
 function App() {
   return (
     <div className="App">
-      <MuiLink />
+      <MuiNavbar />
     </div>
   );
 }
--- src/components/MuiNavbar.tsx
+++ src/components/MuiNavbar.tsx
@@ -1,15 +1,32 @@
-import { CatchingPokemon } from "@mui/icons-material";
+import {
+  CatchingPokemon,
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+} from "@mui/icons-material";
 import {
   AppBar,
   Button,
   IconButton,
+  Menu,
+  MenuItem,
   Stack,
   Toolbar,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 const MuiNavbar = () => {
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -34,10 +51,41 @@ const MuiNavbar = () => {
           <Button variant="outlined" href="#" color="inherit">
             About
           </Button>
+          <Button
+            variant="outlined"
+            href="#"
+            color="inherit"
+            id="resources-button"
+            onClick={handleClick}
+            aria-controls={open ? "resources-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            endIcon={open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+          >
+            Resources
+          </Button>
           <Button variant="outlined" href="#" color="inherit">
             Login
           </Button>
         </Stack>
+        <Menu
+          id="resources-menu"
+          anchorEl={anchorEl}
+          open={open}
+          MenuListProps={{ "aria-labelledby": "resources-button" }}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <MenuItem onClick={handleClose}>Blog</MenuItem>
+          <MenuItem onClick={handleClose}>Podcast</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
