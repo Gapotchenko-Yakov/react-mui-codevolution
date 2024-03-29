c304bf8476967bf1a47245522c52a05c2b22a186
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-03-29 21:43:06 +0300

    Add List demo.

---
+++ src/components/MuiList.tsx
@@ -0,0 +1,59 @@
+import { Mail } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Icon,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import React from "react";
+
+const MuiList = () => {
+  return (
+    <Box sx={{ width: "400px", bgcolor: "#dddddd" }}>
+      <List>
+        <ListItem>
+          <ListItemIcon>
+            <ListItemAvatar>
+              <Avatar>
+                <Mail />
+              </Avatar>
+            </ListItemAvatar>
+          </ListItemIcon>
+          <ListItemText primary="List item 1" secondary="Secondary text" />
+        </ListItem>
+        <Divider />
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <ListItemAvatar>
+                <Avatar>
+                  <Mail />
+                </Avatar>
+              </ListItemAvatar>
+            </ListItemIcon>
+            <ListItemText primary="List item 2" secondary="Secondary text" />
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+        <ListItem>
+          <ListItemIcon>
+            <ListItemAvatar>
+              <Avatar>
+                <Mail />
+              </Avatar>
+            </ListItemAvatar>
+          </ListItemIcon>
+          <ListItemText primary="List item 3" secondary="Secondary text" />
+        </ListItem>
+      </List>
+    </Box>
+  );
+};
+
+export default MuiList;
