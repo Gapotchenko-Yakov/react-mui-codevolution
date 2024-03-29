53e164f930f97a3cc7a560c9edb1eeb8c273adbb
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-03-29 20:43:01 +0300

    Add SpeedDial demo.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import "./App.css";
 import MuiBreadcrumbs from "./components/MuiBreadcrumbs";
 import MuiDrawer from "./components/MuiDrawer";
 import MuiNavbar from "./components/MuiNavbar";
+import MuiSpeedDial from "./components/MuiSpeedDial";
 
 function App() {
   return (
     <div className="App">
-      <MuiNavbar />
-      <MuiBreadcrumbs />
-      <MuiDrawer />
+      <MuiSpeedDial />
     </div>
   );
 }
---
+++ src/components/MuiSpeedDial.tsx
@@ -0,0 +1,23 @@
+import { Edit, FileCopyOutlined, Print, Share } from "@mui/icons-material";
+import { SpeedDial, SpeedDialAction, SpeedDialIcon } from "@mui/material";
+import React from "react";
+
+const MuiSpeedDial = () => {
+  return (
+    <SpeedDial
+      ariaLabel="Navogation speed dial."
+      sx={{ position: "absolute", bottom: 16, right: 16 }}
+      icon={<SpeedDialIcon openIcon={<Edit />} />}
+    >
+      <SpeedDialAction
+        icon={<FileCopyOutlined />}
+        tooltipTitle="Copy"
+        tooltipOpen
+      />
+      <SpeedDialAction icon={<Print />} tooltipTitle="Print" tooltipOpen />
+      <SpeedDialAction icon={<Share />} tooltipTitle="Share" tooltipOpen />
+    </SpeedDial>
+  );
+};
+
+export default MuiSpeedDial;
